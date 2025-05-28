07e01465d3bd4f8934acb15745c04a248da3bf88
Livia123456 <122884168+Livia123456@users.noreply.github.com>
2025-05-28 11:54:10 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,11 +8,11 @@ app.use(cors());
 app.use(express.json());
 
 
-app.post('/getBeers', async (req, res) => {
+app.get('/getBeers', async (req, res) => {
 
     try {
 
-        const body =  {
+        const body = {
             "name": [""],
             "category": ["Öl & Cider"],
             "subcategory": [],
@@ -23,24 +23,67 @@ app.post('/getBeers', async (req, res) => {
             "rank": [1],  // Hur produkten rankas i vår databas just nu
             "articlenbr": [], // (Inte interval)
             "sortOrder": ["name", "asc"], // Anger hur resultatet ska sorteras; Första är ett värde (en av de andra
-                                         // parametrarna utom maxItems) och andra är antingen "desc" (sjunkande)
-                                         // eller "asc" (stigande). Default är ["apk", "desc"]. Endast värde ger "desc"
+            // parametrarna utom maxItems) och andra är antingen "desc" (sjunkande)
+            // eller "asc" (stigande). Default är ["apk", "desc"]. Endast värde ger "desc"
             "maxItems": 0  // maxItems = 0 ger alla produkter
-            }
+        }
 
         //const resp = await fetch('https://api.apkollen.se', JSON.stringify(body));
 
-    const {data} = await axios.post('https://api.apkollen.se', body);
+        const { data } = await axios.post('https://api.apkollen.se', body);
 
-    res.json(data);
-        
+        res.json(data);
+
     } catch (error) {
-        
+
     }
 
 })
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/getAstro/:year', async (req, res) => {
+    const {year} = req.params["year"];
+
+    console.log(year);
+    try {
+        const headers = {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': 'QzktiGgKjA4pJ9cUkscEr2M51omF1OoM1A8S6dS4'
+            }
+        };
+        const body = {
+            "year": 1998,
+            "month": 3,
+            "date": 3,
+            "hours": 10,
+            "minutes": 30,
+            "seconds": 0,
+            "latitude": 17.38405,
+            "longitude": 78.45636,
+            "timezone": 5.5,
+            "config": {
+                "observation_point": "topocentric",
+                "ayanamsha": "tropical",
+                "language": "en"
+            }
+        }
+        const { data } = await axios.post('https://json.freeastrologyapi.com/western/planets', body, headers);
+
+        res.json(data);
+
+    } catch (error) {
+
+    }
+
+})
+
+app.get('/:id', function (req, res) {
+    console.log(req.params['id']);
+    res.send();
+});
+
+app.listen(port, (err) => {
+    if (err) console.log(err);
+    console.log(`Example app listening on port ${port}`)
 })
