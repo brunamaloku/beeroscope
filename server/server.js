b648883811930e72b6e3723789c9ce30a14e42ec
Livia123456 <122884168+Livia123456@users.noreply.github.com>
2025-05-28 17:31:16 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,10 +23,10 @@ app.get('/getBeers', async (req, res) => {
             "rank": [1],
             "articlenbr": [],
             "sortOrder": ["name", "asc"],
-            "maxItems": 0 
-            }
+            "maxItems": 0
+        }
 
-    const {data} = await axios.post('https://api.apkollen.se', body);
+        const { data } = await axios.post('https://api.apkollen.se', body);
 
     } catch (error) {
         console.log(error);
@@ -34,47 +34,76 @@ app.get('/getBeers', async (req, res) => {
 
 })
 
+async function getBeer(sign) {
+    //TODO flytta ölanropo + logik hit 
 
-app.get('/getAstro', async (req, res) => {
-    const year = parseInt(req.query.year);
-    const month = parseInt(req.query.month);
-    const day = parseInt(req.query.day);
+    const body = {
+        "name": [""],
+        "category": ["Öl & Cider"],
+        "subcategory": [],
+        "volume": [],
+        "alcvol": [0],
+        "price": [],
+        "apk": [0],
+        "rank": [1],
+        "articlenbr": [],
+        "sortOrder": ["name", "asc"],
+        "maxItems": 0
+    }
 
-    console.log(year)
-    try {
-        const headers = {
-            headers: {
-                'Content-Type': 'application/json',
-                'x-api-key': 'wWNBWbaWhs1q5QDEo7LPM4RcaIm2Kv553ZWknDGE'
-            }
-        };
-        const body = {
-            "year": year,
-            "month": month,
-            "date": day,
-            "hours": 1,
-            "minutes": 30,
-            "seconds": 0,
-            "latitude": 55.58333,
-            "longitude": 13.00073,
-            "timezone": 2,
-            "config": {
-                "observation_point": "topocentric",
-                "ayanamsha": "tropical",
-                "language": "en"
-            }
+    const { data } = await axios.post('https://api.apkollen.se', body);
+    console.log(data);
+    return data;
+}
+
+
+async function getSign({ month, year, day }) {
+    const headers = {
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': 'wWNBWbaWhs1q5QDEo7LPM4RcaIm2Kv553ZWknDGE'
         }
-        //console.log(scorpioRespons);
-        //const { data } = await axios.post('https://json.freeastrologyapi.com/western/planets', body, headers);
+    };
+    const body = {
+        "year": year,
+        "month": month,
+        "date": day,
+        "hours": 1,
+        "minutes": 30,
+        "seconds": 0,
+        "latitude": 55.58333,
+        "longitude": 13.00073,
+        "timezone": 2,
+        "config": {
+            "observation_point": "topocentric",
+            "ayanamsha": "tropical",
+            "language": "en"
+        }
+    }
 
-        // const sign = data.output[1].zodiac_sign.name.en;
+    //const { data } = await axios.post('https://json.freeastrologyapi.com/western/planets', body, headers);
+    // return data.output[1].zodiac_sign.name.en;
 
-        const sign = "Scorpio"        
+    return "Scorpio";
+}
 
-        res.json(sign);
+
+app.get('/get', async (req, res) => { //döp om till bara "/" o så har vi bara en endpoint?
+    try {
+        const year = parseInt(req.query.year);
+        const month = parseInt(req.query.month);
+        const day = parseInt(req.query.day);
+
+        const sign = await getSign(year, month, day);
+        const beerdata = await getBeer(sign);
+
+        res.json({
+            'sign' : sign, 
+            'beer' : beerdata
+        });
 
     } catch (error) {
-
+        console.log(error);
     }
 
 })
