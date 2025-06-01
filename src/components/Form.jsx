7c358ed6dd08bf6052259e5d523670e40f3e1029
Brunillda Maloku <brunamaloku@gmail.com>
2025-06-01 23:57:12 +0200

    fixade utseendet

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,23 @@
 import { useState, useEffect, React } from 'react'
 import axios from 'axios'
 
-
-
 const Form = ({setDate, setName, setFormSent}) => {
 
     function ButtonClick() {
+        console.log()
         setFormSent(true);
     }
 
     return (
         <>
             <div>
-                <form className="form-inline" action="submit" />
+                <form className="form-inline form-center" action="submit">
                 <label htmlFor="name" className="sr-only">Namn</label>
                 <input
                     type="text"
                     className="form-control mb-2 mr-sm-2"
                     id="name"
-                    placeholder="Namn"
+                    placeholder="Witchy Bitchy"
                     onChange={(e) => setName(e.target.value)}></input>
                 <label htmlFor="date" className="sr-only">Födelsedatum</label>
                 <input
@@ -27,7 +26,8 @@ const Form = ({setDate, setName, setFormSent}) => {
                     id="date"
                     placeholder="Date"
                     onChange={(e) => setDate(e.target.value)}></input>
-                <button type="submit" className="btn btn-primary mb-2" onClick={ButtonClick}>Submit</button>
+                <button type="submit" className="btn mb-2" onClick={ButtonClick}>Submit</button>
+                </form>
             </div>
         </>
     )
