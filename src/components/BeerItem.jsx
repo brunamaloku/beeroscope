e2198959b01b61294f9060e2ca5e3dce25834fbf
Livia123456 <122884168+Livia123456@users.noreply.github.com>
2025-06-02 12:03:15 +0200

    beer-item design update

--- src/components/BeerItem.jsx
+++ src/components/BeerItem.jsx
@@ -23,7 +23,7 @@ const BeerItem = ({ data, setLikedItems, likedItems }) => {
                 <h3>{sign} den {dateString}</h3>
                 <img src={data.image} alt={beer.name} className="beerImage" />
                 <div className="like-div d-flex p-2 bd-highlight align-items-center">
-                <p className="mb-0">Just idag behöver du som {sign} en stärkande {beer.subcategory}. En {beer.volume}cl  {beer.name} kommer passa dig utmärkt en dag som denna!</p>
+                <p className="mb-0 beer-text">Just idag behöver du som {sign} en stärkande {beer.subcategory}. En {beer.volume}cl  {beer.name} kommer passa dig utmärkt en dag som denna!</p>
                 <HeartButton setLikedItems={setLikedItems} likedItems={likedItems} item={data}/>
                 </div>
             </div>
