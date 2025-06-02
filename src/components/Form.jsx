e163f4634bec14512a16df179a79e327753ecd75
Livia123456 <122884168+Livia123456@users.noreply.github.com>
2025-06-02 16:30:07 +0200

    errorhandling on date

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,10 +10,15 @@ const Form = ({setDate, date, setName, setResponseItem, responseItem}) => {
         if (date == undefined) {
             return;
         }
+        console.log()
     
         const year = date.substring(0, 4)
         const month = date.substring(5, 7);
         const day = date.substring(8);
+
+        if (year == "" || month == "" || day == "") {
+            return;
+        }
     
         var today = new Date();
         today.setDate(today.getDate() + offSet);
